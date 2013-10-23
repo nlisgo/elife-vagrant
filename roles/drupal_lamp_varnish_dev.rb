40a80935d0939418d7aced19b5b7ed850cfef80a
Ruth Ivimey-Cook <r.ivimeycook@elifesciences.org>
2013-10-23 16:19:56 +0100

    No longer need drupal_apps or apache_backend, reorder roles slightly.

--- roles/drupal_lamp_varnish_dev.rb
+++ roles/drupal_lamp_varnish_dev.rb
@@ -2,14 +2,17 @@ name "lamp_varnish_drupal_dev"
 description "A LAMP + Varnish + Memcached stack for Drupal similar to Mercury
 with development tools."
 run_list(
+  "role[mysql_server]",
   "role[apache2_mod_php]",
-  "role[apache2_backend]",
+  
+  #"role[apache2_backend]",
+  
   "role[drupal]",
+  "recipe[drush::head]",
+
   #"role[drupal_dev]",
   #"role[memcached]",
-  "role[mysql_server]",
   #"role[varnish_frontend]",
-  "recipe[drupal::drupal_apps]",
-  "recipe[drush::head]",
+  #"recipe[drupal::drupal_apps]",
 )
 # TODO Add recipe to create dev sites via Drush make.
